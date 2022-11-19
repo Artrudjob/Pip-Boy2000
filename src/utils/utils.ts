06dd81574d6e96807105a1e43c69c4967b825666
Artrudjob <Artrudjob@yandex.ru>
2022-11-19 17:25:59 +0300

    feat: add function getTime

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,8 @@
 export function timeout(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+export function getTime() {
+    const date = new Date();
+    return `${date.getHours()}:${date.getMinutes() < 10? "0" : ""}${date.getMinutes()}`;
+}
